20f43ac104a9864bdac25a0df1667faa6b8c215f
Brendan Baldwin <brendan@usergenic.com>
2017-09-29 15:13:25 -0700

    Added parameter documentation for the appMapper argument of type ExpressAppMapper.

--- src/start_server.ts
+++ src/start_server.ts
@@ -102,8 +102,8 @@ export interface ServerOptions {
   additionalRoutes?: Map<string, express.RequestHandler>;
 }
 
-export type ExpressServerRemap =
-    (app: express.Express, options: ServerOptions) => Promise<express.Express>;
+export type ExpressAppMapper = (app: express.Express, options: ServerOptions) =>
+    Promise<express.Express>;
 
 function applyDefaultServerOptions(options: ServerOptions) {
   const withDefaults: ServerOptions = Object.assign({}, options);
@@ -122,25 +122,33 @@ function applyDefaultServerOptions(options: ServerOptions) {
 }
 
 /**
+ * @param {ServerOptions} options used to configure the generated polyserve app
+ *     and server.
+ * @param {ExpressAppMapper} appMapper optional mapper function which is called
+ *     with the generated polyserve app and the options used to generate
+ *     it and returns an optional substitution Express app.  This is usually one
+ *     that mounts the original app, to add routes or middleware in advance of
+ *     polyserve's catch-all routes.
  * @return {Promise} A Promise that completes when the server has started.
  * @deprecated Please use `startServers` instead. This function will be removed
  *     in a future release.
  */
 export async function startServer(
-    options: ServerOptions, remap?: ExpressServerRemap): Promise<http.Server> {
-  return (await _startServer(options, remap)).server;
+    options: ServerOptions,
+    appMapper?: ExpressAppMapper): Promise<http.Server> {
+  return (await _startServer(options, appMapper)).server;
 }
 
 async function _startServer(
-    options: ServerOptions, remap?: ExpressServerRemap) {
+    options: ServerOptions, appMapper?: ExpressAppMapper) {
   options = options || {};
   assertNodeVersion(options);
   try {
     let app = getApp(options);
-    if (remap) {
+    if (appMapper) {
       // If the map function doesn't return an app, we should fallback to the
-      // original app, hence the `remap(app) || app`.
-      app = await remap(app, options) || app;
+      // original app, hence the `appMapper(app) || app`.
+      app = await appMapper(app, options) || app;
     }
     const server = await startWithApp(options, app);
     return {app, server};
@@ -200,16 +208,16 @@ export type StartServerResult = MainlineServer | MultipleServersInfo;
  */
 export async function startServers(
     options: ServerOptions,
-    remap?: ExpressServerRemap): Promise<StartServerResult> {
+    appMapper?: ExpressAppMapper): Promise<StartServerResult> {
   options = applyDefaultServerOptions(options);
   const variants = await findVariants(options);
   // TODO(rictic): support manually configuring variants? tracking more
   //   metadata about them besides their names?
   if (variants.length > 0) {
-    return await startVariants(options, variants, remap);
+    return await startVariants(options, variants, appMapper);
   }
 
-  const serverAndApp = await _startServer(options, remap);
+  const serverAndApp = await _startServer(options, appMapper);
   return {
     options,
     kind: 'mainline',
@@ -235,10 +243,10 @@ async function findVariants(options: ServerOptions) {
 async function startVariants(
     options: ServerOptions,
     variants: {name: string, directory: string}[],
-    remap?: ExpressServerRemap) {
+    appMapper?: ExpressAppMapper) {
   const mainlineOptions = Object.assign({}, options);
   mainlineOptions.port = 0;
-  const mainServer = await _startServer(mainlineOptions, remap);
+  const mainServer = await _startServer(mainlineOptions, appMapper);
   const mainServerInfo: MainlineServer = {
     kind: 'mainline',
     server: mainServer.server,
@@ -251,7 +259,7 @@ async function startVariants(
     const variantOpts = Object.assign({}, options);
     variantOpts.port = 0;
     variantOpts.componentDir = variant.directory;
-    const variantServer = await _startServer(variantOpts, remap);
+    const variantServer = await _startServer(variantOpts, appMapper);
     variantServerInfos.push({
       kind: 'variant',
       variantName: variant.name,
