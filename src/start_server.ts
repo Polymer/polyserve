1553fb788febd7897d96b4a094ab90dd033f52ae
Alexander Marks <aomarks@google.com>
2018-04-06 13:39:34 -0700

    Run clang-format.

--- src/start_server.ts
+++ src/start_server.ts
@@ -164,7 +164,7 @@ async function _startServer(
   }
 }
 
-export type ServerInfo = MainlineServer|VariantServer|ControlServer;
+export type ServerInfo = MainlineServer | VariantServer | ControlServer;
 
 export interface PolyserveServer {
   kind: 'control'|'mainline'|'variant';
@@ -206,7 +206,7 @@ export interface MultipleServersInfo {
   servers: PolyserveServer[];
 }
 
-export type StartServerResult = MainlineServer|MultipleServersInfo;
+export type StartServerResult = MainlineServer | MultipleServersInfo;
 
 /**
  * Starts one or more web servers, based on the given options and
